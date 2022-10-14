820b8f8c92e856cebdb65ffca84a96887c788721
siamkarim <siamhossain518@gmail.com>
2022-10-14 12:03:35 +0600

    template inheriate and urls ,view create

---
+++ crud/first_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from first_app import views
+
+app_name = "first_app"
+urlpatterns = [
+    path('', views.index, name= "index"),
+
+]
--- crud/first_app/views.py
+++ crud/first_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    diction ={}
+    return render( request,'first_app/index.html',context=diction)
